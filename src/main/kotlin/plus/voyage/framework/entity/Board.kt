f820e775f9ac6c885fad7047fee3c2201be14b58
nmin11 <nmin1124@gmail.com>
2025-03-01 22:03:55 +0900

    chore: add a board entity

---
+++ src/main/kotlin/plus/voyage/framework/entity/Board.kt
@@ -0,0 +1,29 @@
+package plus.voyage.framework.entity
+
+import jakarta.persistence.*
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+
+@Entity
+@EntityListeners(AuditingEntityListener::class)
+class Board(
+    @Column(nullable = false)
+    var title: String,
+
+    @Column(nullable = false)
+    var content: String
+) {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "boardId")
+    var id: Int? = null
+
+    @CreatedDate
+    @Column(updatable = false)
+    val createdAt: LocalDateTime = LocalDateTime.now()
+
+    @LastModifiedDate
+    var updatedAt: LocalDateTime = LocalDateTime.now()
+}
