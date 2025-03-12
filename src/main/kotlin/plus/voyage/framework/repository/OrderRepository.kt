f5d0cbe688f94a84f1c8e523251c78da7cb3e54f
nmin11 <nmin1124@gmail.com>
2025-03-12 23:54:25 +0900

    chore: add pessimistic lock on weekly popular coffee menu logic

--- src/main/kotlin/plus/voyage/framework/repository/OrderRepository.kt
+++ src/main/kotlin/plus/voyage/framework/repository/OrderRepository.kt
@@ -1,7 +1,9 @@
 package plus.voyage.framework.repository
 
+import jakarta.persistence.LockModeType
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Lock
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
 import plus.voyage.framework.entity.Coffee
@@ -16,5 +18,6 @@ interface OrderRepository : JpaRepository<Order, Long> {
         GROUP BY o.coffee 
         ORDER BY COUNT(o.coffee) DESC
     """)
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     fun findPopularCoffeeSince(@Param("targetDay") targetDay: LocalDateTime, pageable: Pageable): List<Coffee>
 }
